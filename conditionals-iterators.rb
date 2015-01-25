3ed5b35ce275d78ae9f4752485d1dc10835e18a5
sudharshan-ruby <sudharshan.ruby@gmail.com>
2015-01-25 23:49:13 +0530

    Update conditionals-iterators.rb
    Examples on conditionals & iterators

--- conditionals-iterators.rb
+++ conditionals-iterators.rb
@@ -1,3 +1,48 @@
 =begin
-
+	Grade the students based on the marks
+	Grade A > 81 
+	Grade B 60 - 80
+	Grade C 40 - 59
+	Grade F 0 - 39
 =end
+
+marks = 48
+
+while(marks < 40) do puts "marks less than 40 - while" end
+
+puts "Atleast one exdecutiojn" 
+while(marks < 40)
+
+end
+
+begin
+  puts "marks from until"
+end until(marks < 50)
+
+
+if(marks > 81)
+  puts "#{marks} => qualified to Grade A"
+elsif(marks >= 60 && marks <= 80)
+  puts "#{marks} => qualified to Grade B"
+elsif(marks >= 40 && marks <= 59)
+  puts "#{marks} => qualified to Grade C"
+else
+  puts "#{marks} => qualified to Grade F"
+end
+puts "==================="
+calendar_months = ["jan", "feb", "mar", "april"]
+for month in calendar_months do
+  #puts month
+end
+puts "==================="
+calendar_months.each do |month|
+  puts month
+end
+
+
+
+
+
+
+
+
